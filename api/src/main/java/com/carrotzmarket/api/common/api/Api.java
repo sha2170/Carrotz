4492b68555839f71637ff994337c4da6f22a02aa
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-16 14:48:04 +0900

    요청 처리 Api

--- api/src/main/java/com/carrotzmarket/api/common/api/Api.java
+++ api/src/main/java/com/carrotzmarket/api/common/api/Api.java
@@ -11,11 +11,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Api<T> {
 
-    //
-    private Result result;
+    private Result result; // 들어온 요청에 대한 처리 결과
+    private T data; // 요청 결과 데이터
 
-    @Valid // 유효성 검사!
-    private T data;
+    // 응답 성공
+    public static <T> Api<T> OK(T data) {
+        Api<T> api = new Api<>();
+        api.result = Result.OK(); // 성공했음
+        api.data = data; // 요청에 대한 데이터 설정
+        return api;
+    }
 
-
+    // 에러 처리
+    public static Api<Object> ERROR(Result result){
+        Api<Object> api = new Api<>();
+        api.result = result;
+        return api;
+    }
 }
