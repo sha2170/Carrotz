03407f6a1b4caf92a37e2c636a87364ed1d8f9c2
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-18 13:03:16 +0900

    chore: 세션 유지 확인을 위한 설정 추가

--- api/src/main/java/com/carrotzmarket/api/common/api/Api.java
+++ api/src/main/java/com/carrotzmarket/api/common/api/Api.java
@@ -13,10 +13,13 @@ public class Api<T> {
 
     private Result result; // 들어온 요청에 대한 처리 결과
     private T data; // 요청 결과 데이터
+    private String message;
+    private int status;
 
     // 응답 성공
     public static <T> Api<T> OK(T data) {
         Api<T> api = new Api<>();
+        api.status = 200; // HTTP 200 OK
         api.result = Result.OK(); // 성공했음
         api.data = data; // 요청에 대한 데이터 설정
         return api;
@@ -26,6 +29,14 @@ public class Api<T> {
     public static Api<Object> ERROR(Result result){
         Api<Object> api = new Api<>();
         api.result = result;
+        return api;
+    }
+
+    public static <T> Api<T> unauthorized(String message) {
+        Api<T> api = new Api<>();
+        api.status = 401; // HTTP 401 Unauthorized
+        api.message = message;
+        api.data = null;
         return api;
     }
 }
