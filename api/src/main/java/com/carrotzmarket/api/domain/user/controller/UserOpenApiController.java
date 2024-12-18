c6e94cc60f9f8626ce9a9305f97c1645e3768593
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-18 10:32:39 +0900

    feat: 사용자의 세션정보 저장 및 로그아웃 기능 추가

--- api/src/main/java/com/carrotzmarket/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/com/carrotzmarket/api/domain/user/controller/UserOpenApiController.java
@@ -5,6 +5,9 @@ import com.carrotzmarket.api.domain.user.business.UserBusiness;
 import com.carrotzmarket.api.domain.user.controller.model.UserLoginRequest;
 import com.carrotzmarket.api.domain.user.controller.model.UserRegisterRequest;
 import com.carrotzmarket.api.domain.user.controller.model.UserResponse;
+import com.carrotzmarket.api.domain.user.controller.model.UserSessionInfo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,10 +28,33 @@ public class UserOpenApiController {
         return userBusiness.register(request); // Api<T> 형식으로 응답
     }
 
-    // 사용자 로그인
-    @PostMapping("/login")
-    public Api<UserResponse> login(@Valid @RequestBody UserLoginRequest request) {
-        return userBusiness.login(request); // Api<T> 형식으로 응답
+   @PostMapping("/login")
+    public Api<UserResponse> login(@Valid @RequestBody UserLoginRequest request, HttpServletRequest httpRequest) {
+
+        // 로그인 검증
+        Api<UserResponse> response = userBusiness.login(request);
+
+        // 세션 생성 & 정보 저장
+        HttpSession session = httpRequest.getSession();
+        UserResponse userResponse = response.getData();
+
+        UserSessionInfo sessionInfo = new UserSessionInfo(
+          userResponse.getId(),
+          userResponse.getLoginId(),
+          userResponse.getEmail()
+        );
+        session.setAttribute("userSession", sessionInfo);
+
+        return response;
+    }
+
+    @PostMapping("/logout")
+    public Api<String> logout(HttpServletRequest httpRequest) {
+        HttpSession session = httpRequest.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+        return Api.OK("성공적으로 로그아웃 하였습니다.");
     }
 }
 
