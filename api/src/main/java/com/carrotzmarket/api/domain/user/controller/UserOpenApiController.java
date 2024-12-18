1ed576e062f3e6c20e478df055e89339d48e06d8
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-18 13:20:53 +0900

    chore: 임포트 항목들 임시 와일드 카드 설정화

--- api/src/main/java/com/carrotzmarket/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/com/carrotzmarket/api/domain/user/controller/UserOpenApiController.java
@@ -10,10 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/open-api/user")
@@ -28,7 +25,7 @@ public class UserOpenApiController {
         return userBusiness.register(request); // Api<T> 형식으로 응답
     }
 
-   @PostMapping("/login")
+    @PostMapping("/login")
     public Api<UserResponse> login(@Valid @RequestBody UserLoginRequest request, HttpServletRequest httpRequest) {
 
         // 로그인 검증
@@ -39,9 +36,9 @@ public class UserOpenApiController {
         UserResponse userResponse = response.getData();
 
         UserSessionInfo sessionInfo = new UserSessionInfo(
-          userResponse.getId(),
-          userResponse.getLoginId(),
-          userResponse.getEmail()
+                userResponse.getId(),
+                userResponse.getLoginId(),
+                userResponse.getEmail()
         );
         session.setAttribute("userSession", sessionInfo);
 
@@ -51,10 +48,20 @@ public class UserOpenApiController {
     @PostMapping("/logout")
     public Api<String> logout(HttpServletRequest httpRequest) {
         HttpSession session = httpRequest.getSession(false);
-        if(session != null) {
+        if (session != null) {
             session.invalidate();
         }
         return Api.OK("성공적으로 로그아웃 하였습니다.");
+    }
+
+    // 세션 상태 확인 API
+    @GetMapping("/session-status")
+    public Api<String> checkSessionStatus(HttpServletRequest httpRequest) {
+        HttpSession session = httpRequest.getSession(false); // 기존 세션 가져오기
+        if (session == null || session.getAttribute("userSession") == null) {
+            return Api.unauthorized("Session expired or not found"); // 세션 없음
+        }
+        return Api.OK("Session is active"); // 세션 유효
     }
 }
 
