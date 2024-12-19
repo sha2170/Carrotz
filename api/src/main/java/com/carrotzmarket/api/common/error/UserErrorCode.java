470d89ce069052998bc38f30b4d633e997929b1d
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-19 21:06:19 +0900

    chore: 유저 에러 코드 추가

--- api/src/main/java/com/carrotzmarket/api/common/error/UserErrorCode.java
+++ api/src/main/java/com/carrotzmarket/api/common/error/UserErrorCode.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 public enum UserErrorCode implements ErrorCodeInterface{
     // (HttpStatusCode, 우리서비스 에러코드, 코드 설명)
     USER_NOT_FOUND(400, 1404, "사용자를 찾을 수 없음"),
+    USER_ALREADY_EXIST(400, 1405, "이미 존재하는 사용자"),
     ;
 
     private final Integer httpStatusCode;
