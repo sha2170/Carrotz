19e997bcacf3a38a8777cbddbb35b84d34fc664f
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-19 21:08:58 +0900

    refactor: 리팩토링

--- api/src/main/java/com/carrotzmarket/api/domain/region/service/RegionService.java
+++ api/src/main/java/com/carrotzmarket/api/domain/region/service/RegionService.java
@@ -8,6 +8,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
