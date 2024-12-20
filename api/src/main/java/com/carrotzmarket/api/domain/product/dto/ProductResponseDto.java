ef950b4decfe4712f03aca009fb46a738356b18c
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2024-12-20 13:23:35 +0900

    feat: 카테고리 및 상품 관련 엔티티 및 서비스 추가

--- api/src/main/java/com/carrotzmarket/api/domain/product/dto/ProductResponseDto.java
+++ api/src/main/java/com/carrotzmarket/api/domain/product/dto/ProductResponseDto.java
@@ -1,5 +1,7 @@
 package com.carrotzmarket.api.domain.product.dto;
 
+import com.carrotzmarket.api.domain.category.dto.CategoryDto;
+import com.carrotzmarket.db.product.ProductEntity;
 import com.carrotzmarket.db.product.ProductStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +15,27 @@ public class ProductResponseDto {
     private String title;
     private String description;
     private int price;
-    private Long categoryId;
     private Long userId;
     private Long regionId;
+    private CategoryDto category;
     private ProductStatus status;
+
+    public ProductResponseDto(ProductEntity product) {
+        this.id = product.getId();
+        this.title = product.getTitle();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+        this.userId = product.getUserId();
+        this.regionId = product.getRegionId();
+        this.status = product.getStatus();
+
+        if (product.getCategory() != null) {
+            this.category = new CategoryDto(
+                    product.getCategory().getId(),
+                    product.getCategory().getName(),
+                    product.getCategory().getDescription(),
+                    product.getCategory().isEnabled()
+            );
+        }
+    }
 }
